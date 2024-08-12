ca7cc008a265535f9bce65ab8ad9950f17ea8839
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-08-12 23:52:20 +0900

    [fix] TodoList 오류 해결

--- 04-Component/next-component-app/components/todo-list.tsx
+++ 04-Component/next-component-app/components/todo-list.tsx
@@ -1,5 +1,3 @@
-import { NextPage } from 'next';
-
 import { TodoType } from '@/interface/todo';
 
 type TodoListProps = {
@@ -7,27 +5,25 @@ type TodoListProps = {
   removeTodo: (index: number) => void;
 };
 
-const TodoList = ({ todos,removeTodo }: TodoListProps) => {
+const TodoList = ({ todos, removeTodo }: TodoListProps) => {
   return (
-    <>
-      <ul>
-        {todos.map((todo: TodoType, index: number) => (
-          <li
-            key={index}
-            className="flex items-center justify-between border-b border-gray-300 py-2"
+    <ul>
+      {todos.map((todo: TodoType, index: number) => (
+        <li
+          key={index}
+          className="flex items-center justify-between border-b border-gray-300 py-2"
+        >
+          <span>{todo.title}</span>
+          <button
+            type="button"
+            onClick={(e) => removeTodo(index)}
+            className="text-red-500 hover:text-red-600"
           >
-            <span>{todo.title}</span>
-            <button
-              type="button"
-              onClick={(e)=>removeTodo(index)}
-              className="text-red-500 hover:text-red-600"
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- 04-Component/next-component-app/components/todo-template.tsx
+++ 04-Component/next-component-app/components/todo-template.tsx
@@ -1,17 +1,10 @@
-import {NextPage} from 'next';
+import React from "react";
 
-import React, {ReactNode} from 'react';
-import TodoRegist from './todo-regist';
-import TodoList from './todo-list';
-
-interface TodoTemplateProps {
-  children: ReactNode;
-}
-
-const TodoTemplate:NextPage<TodoTemplateProps> = ({children}) => {
+const TodoTemplate = ({children}: {children:React.ReactNode}) => {
   return (
     <div className="container mx-auto max-w-md p-4">
       <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      {children}
     </div>
   );
 };

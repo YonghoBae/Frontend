ed4435a538e1e02974e103ea688d977f54a95948
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-08-21 15:49:27 +0900

    socket통신&gpt 실시간 통신

---
+++ Final/next-platform-app/interfaces/message.ts
@@ -0,0 +1,7 @@
+export type Message = {
+    member_id:number;
+    name:string;
+    profile:string;
+    message:string;
+    send_date:string;
+}
--- Final/next-platform-app/pages/bot/index.tsx
+++ Final/next-platform-app/pages/bot/index.tsx
@@ -1,4 +1,81 @@
+import { Message } from "@/interfaces/message";
+import { socket } from "@/library/socket";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 const Bot = () => {
+  const router = useRouter();
+
+  const [memberId, setMemberId] = useState<number>(1);
+
+  const [message, setMessage] = useState<string>('');
+
+  const [messageList, setMessageList] = useState<Message[]>([]);
+
+  //최초 마운팅 되는 시점에 실행되는 useEffect
+  //next.config.mjs 파일 내 reactStrictMode(엄격모드)값을 false로 변경해야 정확히 1회만 실행
+  //채팅서버와 연결되는 클라이언트 채팅 소켓 객체 생성 및 각종 채팅 이벤트 기능 구현영역
+  useEffect(() => {
+    //서버소켓 연결
+    socket.connect();
+    //서버소켓과 연결이 완료되면 실행되는 이벤트처리함수
+    //서버 소켓과 연결이 완료되면 자동으로 client 소켓에서 connect이벤트가 실행되고
+    //connect이벤트가 실행되면 처리할 이벤트 처리할 기능 구현
+    socket.on('connect', () => {
+      console.log('정상적으로 서버소켓과 연결 완료');
+    });
+
+    //disconnect 이벤트는 서버소켓에 끊어진 경우 발생하는 이벤트
+    //서버와의 연결소켓이 끊어진경우 처리할 기능을 핸들러함수에서 처리
+    //소켓 시스템 이벤트
+    socket.on('disconnect', () => {
+      console.log('서버소켓 연결 종료');
+    });
+
+    //개발자 정의 클라이언트 소켓 이벤트 수신기 정의
+    //socket.on('클라이언트 이벤트 수신기명',서버에서 전달해준 데이터를 받는 함수 정의)
+    socket.on('gptMessage', (msg: Message) => {
+      console.log('서버소켓에서 전달된 데이터 확인-receiveAll:', msg);
+      setMessageList(prev => [...prev, msg]);
+    });
+
+    //해당 채팅 컴포넌트가 화면에서 사라질때(언마운팅시점)
+    //소켓관련 이벤트를 모두 제거해줘야함. 그렇지 않으면 메시지를 여러번 수신할수있음
+    return ()=>{
+      //socket.off(클라이언트 이벤트 수신기명); // 이벤트 제거
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('gptMessage');
+    }
+  }, []);
+
+  //useEffect훅은 CSR환경에서 작동되고 useRouter훅은 SSR/CSR순서로 2번 작동
+  //useEffect훅에서 useRouter훅을 이용해 URL키값이 추출안되는 문제는 useRouter.isReady값을 이용해 해결가능
+  //useRouter.isReady 값이 기본은 false->true로 변경되는 시점에 관련 기능 구현하면 됨
+  useEffect(() => {
+    console.log('현재 URL주소에서 사용자 고유번호 추출:', router.query.id);
+
+    if (router.query.id != undefined) {
+      setMemberId(Number(router.query.id));
+    }
+  }, [router.isReady]);
+
+  const sendMessage = () => {
+    const msgData = {
+      member_id: memberId,
+      name: `사용자-${memberId.toString()}`,
+      profile: `http://localhost:5000/img/user${memberId.toString()}.png`,
+      message: message,
+      send_date: Date.now().toString(),
+    };
+    //채팅서버소켓으로 메시지를 전송
+    //socket.emit('서버이벤트명',메시지데이터);
+    socket.emit('gpt', msgData);
+
+    //메시지 입력박스 초기화
+    setMessage('');
+  };
+
   return (
     <div className="flex h-screen antialiased text-gray-800 mt-14 pb-10">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
@@ -8,8 +85,46 @@ const Bot = () => {
             <div className="flex flex-col h-full overflow-x-auto mb-4">
               <div className="flex flex-col h-full">
                 <div className="grid grid-cols-12 gap-y-2">
-                  {/* 왼쪾 다른 사용자 메시지 출력 영역 */}
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  {messageList.map((msg, index) =>
+                    msg.member_id === memberId ? (
+                      <div
+                        key={index}
+                        className="col-start-6 col-end-13 p-3 rounded-lg"
+                      >
+                        <div className="flex items-center justify-start flex-row-reverse">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                            {msg.name}
+                          </div>
+                          <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                            <div>{msg.message}</div>
+                            <div className="absolute w-[200px] text-right text-xs bottom-0 right-0 -mb-5 text-gray-500">
+                              {msg.name} {msg.send_date}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      <div
+                        key={index}
+                        className="col-start-1 col-end-8 p-3 rounded-lg"
+                      >
+                        <div className="flex flex-row items-center">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                            {msg.name}
+                          </div>
+                          <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                            <div>{msg.message}</div>
+                            <div className="absolute w-[200px] text-right text-xs bottom-0 right-0 -mb-5 text-gray-500">
+                              {msg.name} {msg.send_date}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ),
+                  )}
+
+                  {/* 왼쪽 다른 사용자 메시지 출력 영역 */}
+                  {/* <div className="col-start-1 col-end-8 p-3 rounded-lg">
                     <div className="flex flex-row items-center">
                       <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
                         A
@@ -18,10 +133,10 @@ const Bot = () => {
                         <div>Hey How are you today?</div>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
 
                   {/* 오른쪽 본인 메시지 출력영역 */}
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                  {/* <div className="col-start-6 col-end-13 p-3 rounded-lg">
                     <div className="flex items-center justify-start flex-row-reverse">
                       <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
                         A
@@ -32,7 +147,7 @@ const Bot = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
@@ -59,6 +174,11 @@ const Bot = () => {
                 <div className="relative w-full">
                   <input
                     type="text"
+                    name={message}
+                    value={message}
+                    onChange={e => {
+                      setMessage(e.target.value);
+                    }}
                     className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
                   />
                 </div>
@@ -66,7 +186,11 @@ const Bot = () => {
 
               {/* 메시지 전송버튼 영역 */}
               <div className="ml-4">
-                <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                <button
+                  type="button"
+                  onClick={sendMessage}
+                  className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                >
                   <span>Send</span>
                   <span className="ml-2">
                     <svg
